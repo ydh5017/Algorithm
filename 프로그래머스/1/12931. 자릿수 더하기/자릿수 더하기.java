419d498f3a1aaf1db190d392f8f14f3a39cbb4ec
ydh5017 <64765991+ydh5017@users.noreply.github.com>
2024-04-26 11:50:06 +0900

    [level 1] Title: 자릿수 더하기, Time: 1.52 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,6 +3,16 @@ import java.util.stream.Stream;
 
 public class Solution {
     public int solution(int n) {
+//         int answer = 0;
+
+//         while(true){
+//             answer+=n%10;
+//             if(n<10)
+//                 break;
+
+//             n=n/10;
+//         }
+//      return answer;
         return Stream.of(String.valueOf(n).split("")).mapToInt(Integer::parseInt).sum();
     }
 }
