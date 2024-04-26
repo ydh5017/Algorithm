d85506d9844e77dc0708df23b8569e9dafa5e897
ydh5017 <64765991+ydh5017@users.noreply.github.com>
2024-04-26 11:43:20 +0900

    [level 0] Title: 몫 구하기, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        if (valid(num1) && valid(num2)) {
+            answer = num1 / num2;
+        }
+        return answer;
+    }
+    
+    private boolean valid(int number) {
+        return number >= 0 && number <= 100;
+    }
+}
