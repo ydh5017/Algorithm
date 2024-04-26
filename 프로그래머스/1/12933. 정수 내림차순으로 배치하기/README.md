0455c31022da44f47a855d37754ebef3d8fc13e8
ydh5017 <64765991+ydh5017@users.noreply.github.com>
2024-04-26 15:39:42 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.49 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 정수 내림차순으로 배치하기 - 12933 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933#) 
 
 ### 성능 요약
 
-메모리: 72.7 MB, 시간: 4.01 ms
+메모리: 72.7 MB, 시간: 0.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 26일 15:18:38
+2024년 04월 26일 15:39:40
 
 ### 문제 설명
 
--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -3,6 +3,7 @@ import java.util.stream.Stream;
 import java.util.stream.Collectors;
 class Solution {
     public long solution(long n) {
+        /* case 1
         int[] arr = Stream.of(String.valueOf(n).split("")).mapToInt(Integer::parseInt).toArray();
         for (int i = 0; i < arr.length-1; i++) {
             for (int j = i+1; j < arr.length; j++) {
@@ -15,6 +16,15 @@ class Solution {
         }
         String str = Arrays.stream(arr).mapToObj(String::valueOf).collect(Collectors.joining());
         
-        return Long.parseLong(str);
+        return Long.parseLong(str);*/
+        
+        /* case 2 */
+        String[] list = String.valueOf(n).split("");
+        Arrays.sort(list);
+
+        StringBuilder sb = new StringBuilder();
+        for (String aList : list) sb.append(aList);
+
+        return Long.parseLong(sb.reverse().toString());
     }
 }
