8ae2f001e9d2ec15addbf095210a2ac3c6674d9c
ydh5017 <64765991+ydh5017@users.noreply.github.com>
2024-04-27 21:04:58 +0900

    [level 1] Title: 하샤드 수, Time: 2.11 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        String str = String.valueOf(x);
+        int[] arr = Arrays.stream(str.split("")).mapToInt(Integer::parseInt).toArray();
+        
+        for(int digit : arr) {
+            sum = sum + digit;
+        }
+        return x % sum == 0 ? true : false;
+    }
+}
