d037a695a1aea563d0a35eb7eba9a47a0eae17ca
ydh5017 <64765991+ydh5017@users.noreply.github.com>
2024-04-29 10:59:07 +0900

    [level 1] Title: 내적, Time: 2.39 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -1,9 +1,6 @@
+import java.util.stream.IntStream;
 class Solution {
     public int solution(int[] a, int[] b) {
-        int answer = 0;
-        for(int i = 0; i < a.length; i++){
-            answer += a[i] * b[i];
-        }
-        return answer;
+        return IntStream.range(0, a.length).map(i -> a[i] * b[i]).sum();
     }
 }
