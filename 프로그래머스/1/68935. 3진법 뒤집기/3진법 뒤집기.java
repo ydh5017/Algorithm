7de15cfb350682b769402777e211897abe3351d4
ydh5017 <64765991+ydh5017@users.noreply.github.com>
2024-04-29 16:01:06 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.01 ms, Memory: 68 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -2,6 +2,7 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         while (n > 0) {
+            // n/3 = 몫, n%3 = 나머지 = 3진법 거꾸로
             answer = answer * 3 + n % 3;
             n /= 3;
         }
