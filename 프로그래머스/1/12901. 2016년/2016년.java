6aacb531a8601fb035967ff06bf58e06b992e5b9
ydh5017 <64765991+ydh5017@users.noreply.github.com>
2024-04-30 17:13:56 +0900

    [level 1] Title: 2016년, Time: 1.33 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+class Solution {
+    public String solution(int a, int b) {
+        String[] week = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+        LocalDate date = LocalDate.of(2016, a, b);
+        int dayOfWeek = date.getDayOfWeek().ordinal();
+        return week[dayOfWeek];
+    }
+}
