d69fc3bc981f914736ee6d1392b2d57db3e544a1
ydh5017 <64765991+ydh5017@users.noreply.github.com>
2024-04-30 11:48:58 +0900

    [level 1] Title: K번째수, Time: 2.30 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -9,6 +9,13 @@ class Solution {
                     .skip(commands[i][2]-1)
                     .limit(1).sum();
         }
+        
+        // int[] answer = new int[commands.length];
+        // for(int i=0; i<commands.length; i++){
+        //     int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+        //     Arrays.sort(temp);
+        //     answer[i] = temp[commands[i][2]-1];
+        // }
         return answer;
     }
 }
