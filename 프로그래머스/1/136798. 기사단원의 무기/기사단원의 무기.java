f79001f25ffc0e0a057eab71e6f9539f40380470
ydh5017 <64765991+ydh5017@users.noreply.github.com>
2024-05-01 16:02:36 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.45 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,17 +1,17 @@
 class Solution {
     public int solution(int number, int limit, int power) {
-        int cnt = 0;
+        int cnt = 0;//약수 개수 카운트
         int answer = 0;
-        for (int i = 1; i <= number; i++) {
-            cnt = 0;
-            for (int j = 1; j*j <= i; j++) {
+        for (int i = 1; i <= number; i++) {//기사 수만큼 반복
+            cnt = 0;//약수 개수 초기화
+            for (int j = 1; j*j <= i; j++) {//약수 개수 구하기
                 if (j * j == i) cnt++;
                 else if (i % j == 0) cnt+=2;
             }
-            if (cnt > limit) {
+            if (cnt > limit) {//제한수치 초과 시
                 cnt = power;
             }
-            answer += cnt;
+            answer += cnt;//공격력 합
         }
         return answer;
     }
