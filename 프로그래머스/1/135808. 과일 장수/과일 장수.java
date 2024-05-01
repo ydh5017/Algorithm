b2fce0d3e902ce76e760cdf82ea5b0a93a3a428a
ydh5017 <64765991+ydh5017@users.noreply.github.com>
2024-05-01 10:21:49 +0900

    [level 1] Title: 과일 장수, Time: 0.40 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -3,9 +3,10 @@ import java.util.stream.IntStream;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        Arrays.sort(score);
+        Arrays.sort(score); //정렬
 
         for(int i = score.length; i >= m; i -= m){
+            //m개 단위로 자르며 맨 앞의 요소에 m을 곱해서 answer에 더해줌(정렬해서 맨 앞이 가장 낮은 점수)
             answer += score[i - m] * m;
         }
         return answer;
