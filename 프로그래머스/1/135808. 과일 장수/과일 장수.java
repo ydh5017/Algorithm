a3fddaf615b91727e8546c6a867d3eecc2f52c0b
ydh5017 <64765991+ydh5017@users.noreply.github.com>
2024-05-01 10:18:43 +0900

    [level 1] Title: 과일 장수, Time: 0.39 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -2,13 +2,11 @@ import java.util.*;
 import java.util.stream.IntStream;
 class Solution {
     public int solution(int k, int m, int[] score) {
-        Integer[] scoreArr = Arrays.stream(score).boxed().toArray(Integer[]::new);
         int answer = 0;
-        Arrays.sort(scoreArr, Collections.reverseOrder());
-        for (int i = 0; i < scoreArr.length / m; i++) {
-            int result = IntStream.rangeClosed(0+(i*m), (m-1)+(i*m))
-                    .map(n -> scoreArr[n]).min().getAsInt();
-            answer += result*m;
+        Arrays.sort(score);
+
+        for(int i = score.length; i >= m; i -= m){
+            answer += score[i - m] * m;
         }
         return answer;
     }
