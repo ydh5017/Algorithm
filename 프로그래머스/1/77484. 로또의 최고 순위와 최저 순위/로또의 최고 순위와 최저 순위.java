7992953679670186af32fd30dfc879d6543a59f2
ydh5017 <64765991+ydh5017@users.noreply.github.com>
2024-05-02 09:36:14 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 1.84 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,11 +1,12 @@
 import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int min = (int) IntStream.of(lottos).filter(n -> n > 0).filter(n -> {
-            for (int num : win_nums) if (n == num) return true;
-            return false;
-        }).count();
-        switch (min) {
+        int min = (int) IntStream.of(lottos).filter(n -> n > 0)//로또번호 중 0인 경우 제외
+            .filter(n -> {//당첨번호와 일치하는지 대조
+                for (int num : win_nums) if (n == num) return true;
+                return false;})
+            .count();//일치하는 번호 개수 카운트
+        switch (min) {//일치하는 번호 개수에 따라 순위로 변경
             case 2: min = 5; break;
             case 3: min = 4; break;
             case 4: min = 3; break;
@@ -13,7 +14,7 @@ class Solution {
             case 6: min = 1; break;
             default: min = 6;
         }
-        int max = min - (int) IntStream.of(lottos).filter(n -> n == 0).count();
+        int max = min - (int) IntStream.of(lottos).filter(n -> n == 0).count();//최저순위에 따라 최고순위 계산
         int[] answer = {max == 0 ? 1 : max, min};
         return answer;
     }
