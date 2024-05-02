886156acc049c86b98cca8752780b96c97ed3864
ydh5017 <64765991+ydh5017@users.noreply.github.com>
2024-05-02 10:10:20 +0900

    [level 1] Title: 옹알이 (2), Time: 0.69 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -3,17 +3,17 @@ class Solution {
         // "aya", "ye", "woo", "ma" 4가지 발음만 가능
         int answer = 0;
         for(int i = 0; i < babblings.length; i++) {
-            if(babblings[i].contains("ayaaya") || babblings[i].contains("yeye") || babblings[i].contains("woowoo") || babblings[i].contains("mama")) {
+            if(babblings[i].contains("ayaaya") || babblings[i].contains("yeye") || babblings[i].contains("woowoo") || babblings[i].contains("mama")) {//연속된 발음이 있으면 넘어감
                 continue;
             }
-
+            //허용되는 발음들 변경
             babblings[i] = babblings[i].replace("aya", " ");
             babblings[i] = babblings[i].replace("ye", " ");
             babblings[i] = babblings[i].replace("woo", " ");
             babblings[i] = babblings[i].replace("ma", " ");
             babblings[i] = babblings[i].replace(" ", "");
 
-            if(babblings[i].length()  == 0) answer++;
+            if(babblings[i].length()  == 0) answer++;//발음 가능한 발음이면 카운트
 
         }
         return answer;
